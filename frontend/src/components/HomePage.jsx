4f8189fe101e69e7a7eac7969a886c343c955587
Abdurahman Mohammed <abdorehman@gmail.com>
2023-12-24 23:09:49 -0600

    Add ReactJS useNavigate to redirect user to a specific URL

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,10 +1,26 @@
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    console.log("Your'e loggedout");
+    navigate("/login");
+  }
 
   return (
-    <p className="content-container">
-      Landlords, your <strong>tenant's</strong> satisfaction begins here. TenantCare provides the simplest solution for your tenants to report any maintenance concern they may have.
-    </p>
+    <div>
+      <Link to="/login">
+        Logout
+      </Link>
+      <button className="btn btn-info"
+        onClick={() => logout()}>Logout
+      </button>
+
+      <p className="content-container">
+        Landlords, your <strong>tenant's</strong> satisfaction begins here. TenantCare provides the simplest solution for your tenants to report any maintenance concern they may have.
+      </p>
+    </div>
   );
 
 };

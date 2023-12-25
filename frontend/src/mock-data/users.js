386a8e9df843005b6f8f40ba48fb1614ed63c132
Abdurahman Mohammed <abdorehman@gmail.com>
2023-12-25 10:12:12 -0600

    Add mock data

---
+++ frontend/src/mock-data/users.js
@@ -0,0 +1,13 @@
+export const users = [
+  {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "123"
+  },
+  
+  {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "abc123"
+  }
+]

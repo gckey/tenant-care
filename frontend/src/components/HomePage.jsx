e27aea5cb03f0b730166f18f18f203e64c9e86d4
Abdurahman Mohammed <abdorehman@gmail.com>
2024-01-14 17:52:04 -0600

    Style the CSS for the <div> tag

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
   return (
     <>
     
-    <div className="row">
+    <div className="row container">
       <div className="column side">
         <h2>Project Summary</h2>
         <p>
